a19ec6dccf4f4a67e6861549765895463babe3eb
Itay Bardan <itay.bardan@gmail.com>
2022-09-06 19:41:29 -0400

    add comment

--- src/main/kotlin/Triangle.kt
+++ src/main/kotlin/Triangle.kt
@@ -4,7 +4,7 @@ class Triangle(_name: String, _a: Double, _b: Double, _c: Double) : Shape(_name)
     var a = _a;
     var b = _b;
     var c = _c;
-    var s = (a + b + c) / 2
+    var s = (a + b + c) / 2 // semiperimeter
 
     override fun printDimensions() {
         println("triangle dimensions are ${this.a} ${this.b}, ${this.c}")
